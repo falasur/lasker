7563e9a9f8b50091b8d32bbc73b0a92b6bd2d36b
Fakhrutdinov Ruslan <RFakhrutdinov@korusconsulting.ru>
2023-05-25 10:57:42 +0300

    Adding function to interact with lichess API

---
+++ lasker/functions.py
@@ -0,0 +1,11 @@
+import requests
+from html.parser import HTMLParser
+
+url = 'https://lichess.org/api/games/user/' # API URL
+username = 'R1206' # In the end username must be acquired from config file instead of hardcode. 
+                   # The same goes for the url
+
+def get_last_game_of_user(url, username): # returns PGN of a last game
+    url = url + username
+    game = requests.get(url, params={'max':1})
+    return game.text # Maybe it would be better if function returns a dictionary instead of string PDN
