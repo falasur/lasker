f3bd33f03ddb3e941716f3c5be31437cdf4d56e8
Fakhrutdinov Ruslan <RFakhrutdinov@korusconsulting.ru>
2023-05-25 12:22:12 +0300

    modified working with lichess API

--- lasker/functions.py
+++ lasker/functions.py
@@ -1,4 +1,6 @@
 import requests
+import chess.pgn
+import io
 from html.parser import HTMLParser
 
 url = 'https://lichess.org/api/games/user/' # API URL
@@ -7,5 +9,8 @@ username = 'R1206' # In the end username must be acquired from config file inste
 
 def get_last_game_of_user(url, username): # returns PGN of a last game
     url = url + username
-    game = requests.get(url, params={'max':1})
-    return game.text # Maybe it would be better if function returns a dictionary instead of string PDN
+    g = requests.get(url, params={'max':1})
+    if g.status_code != 200:
+        raise Exception(f'Failed to retrieve the game. Status Code is {g.status_code}.')
+    game = chess.pgn.read_game(io.StringIO(g.text))
+    return game
