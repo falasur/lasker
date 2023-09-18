e2f20e38d036ee155924fab92c3e56e830c4633c
Fakhrutdinov Ruslan <RFakhrutdinov@korusconsulting.ru>
2023-09-18 12:20:29 +0300

    linting

--- lasker/functions.py
+++ lasker/functions.py
@@ -9,7 +9,8 @@ username = 'R1206' # In the end username must be acquired from config file inste
                    # The same goes for the url
 engine = chess.engine.SimpleEngine.popen_uci(r'C:\Users\rfakhrutdinov\stockfish\stockfish-windows-2022-x86-64-avx2.exe') # Should be taken from config
 
-def get_last_game_of_user(url, username): # returns PGN of a last game
+# returns PGN of a last game
+def get_last_game_of_user(url, username): 
     url = url + username
     g = requests.get(url, params={'max':1})
     if g.status_code != 200:
@@ -17,12 +18,14 @@ def get_last_game_of_user(url, username): # returns PGN of a last game
     game = chess.pgn.read_game(io.StringIO(g.text))
     return game
 
-def get_list_of_moves(game): # returns a list of moves
+# returns a list of moves
+def get_list_of_moves(game): 
     moves = []
     for move in game.mainline_moves():
         moves.append(move)
     return moves
 
-def is_best(move, board, engine, *args, depth=18, **kvargs): # checks if a move is the best
+# checks if a move is the best
+def is_best(move, board, engine, *args, depth=18, **kvargs): 
     evaluation = engine.analyse(board, chess.engine.Limit(*args, depth=18, **kvargs))
     return evaluation['pv'][0] == move
